ca82ac3efd54d644e206da53366be43979fff274
jakubkozak <kuba12kozak@gmail.com>
2021-11-03 22:08:42 +0100

    Spring boot project init

---
+++ src/main/java/com/kozak/EnigmaZadanie2/EnigmaZadanie2Application.java
@@ -0,0 +1,13 @@
+package com.kozak.EnigmaZadanie2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmaZadanie2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmaZadanie2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kozak/EnigmaZadanie2/EnigmaZadanie2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kozak.EnigmaZadanie2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnigmaZadanie2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
